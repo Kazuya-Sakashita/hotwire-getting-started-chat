0960c9e15741dc642fd36bd48ae026763f17c06e
Kazuya-Sakashita <kz0508@gmail.com>
2023-12-06 06:52:45 +0900

    message の編集、削除をリアルタイムに反映する

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,4 +7,16 @@ class Message < ApplicationRecord
                                         partial: 'messages/message',
                                         locals: { message: self }
                                         ) }
+
+  after_update_commit -> { broadcast_replace_to(
+                                        "messages",
+                                        target: self,
+                                        partial: 'messages/message',
+                                        locals: { message: self }
+                                        ) }
+
+  after_destroy_commit -> { broadcast_remove_to(
+                                        "messages",
+                                        target: self
+                                        ) }
 end
