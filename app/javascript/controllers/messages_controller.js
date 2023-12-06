f5ee94772c6a500f369ea68f7961abf4f9e27db4
Kazuya-Sakashita <kz0508@gmail.com>
2023-12-07 06:28:14 +0900

    新しいメッセージまでスクロール

---
+++ app/javascript/controllers/messages_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="messages"
+export default class extends Controller {
+  connect() {
+    this.element.scrollTop = this.element.scrollHeight;
+  }
+
+  broadcastMessageTargetConnected(element) {
+    element.scrollIntoView();
+  }
+}
